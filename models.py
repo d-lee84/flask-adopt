273415fd9aa8c56c86dba9123891d4bfdacf2996
David Lee <davidlee980804@gmail.com>
2020-12-11 12:03:40 -0800

    Created model, set up homepage

--- models.py
+++ models.py
@@ -13,3 +13,29 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+
+class Pet(db.Models):
+    """ Model for our pets table """ 
+
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer,
+                   autoincrement=True,
+                   primary_key=True)
+    name = db.Column(db.Text,
+                     nullable=False)
+    species = db.Column(db.Text,
+                        nullable=False)
+    photo_url = db.Column(db.Text,
+                          nullable=False,
+                          default='')
+    age = db.Column(db.Text,
+                    db.CheckContraint(
+                        'age in ("baby", "young", "adult", "senior")'
+                    ),
+                    nullable=False)
+    notes = db.Column(db.Text)
+    available = db.Column(db.Boolean,
+                          nullable=False,
+                          default=True)
